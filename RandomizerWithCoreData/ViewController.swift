b8e344fe34c230b0690afc5c4bced3b6eb325395
Roman <rotach.2000@yandex.ru>
2020-04-09 14:25:03 +0300

    Initial Commit

---
+++ RandomizerWithCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomizerWithCoreData
+//
+//  Created by Rotach Roman on 09.04.2020.
+//  Copyright © 2020 Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
